420d03a604649f36bcea14e36f1049352e104128
Bob Park <hwpark@malgn.com>
2024-08-14 13:45:55 +0900

    feat: 필요한것만 추가중

--- index.js
+++ index.js
@@ -5,15 +5,36 @@ module.exports = {
         node: true,
         browser: true,
     },
-    extends: ["eslint:recommended", "airbnb-base"],
+    extends: [
+        "next",
+        "airbnb",
+        "plugin:react/recommended",
+        "plugin:jsx-a11y/recommended",
+        "plugin:react-hooks/recommended"],
+    plugins: ["react", "react-hooks", "jsx-a11y"],
     parserOptions: {
         ecmaVersion: 12,
         sourceType: "module",
     },
     rules: {
+        "import/extensions": "off",
+        "one-var": "off",
         "indent": ["error", 2],
-        "linebreak-style": ["error", "unix"],
-        "quotes": ["error", "single"],
-        "semi": ["error", "always"]
+        "space-in-paren": "off",
+        "sort-import": "off",
+        "no-restricted-imports": [
+            "error",
+            {
+                "patterns": ["^(..)*"],
+                "paths": [
+                    {
+                        "name": "react",
+                        "importNames": ["default"],
+                        "message": "import React from 'react' makes bundle size larger."
+                    }
+                ]
+            }
+        ],
     }
+
 };
