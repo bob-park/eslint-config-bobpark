aee73a6203ab4eb448bfce6c4060fe82916f4b8c
Bob Park <hwpark@malgn.com>
2024-08-22 12:53:42 +0900

    feat: rules 수정

--- index.js
+++ index.js
@@ -17,11 +17,39 @@ module.exports = {
         sourceType: "module",
     },
     rules: {
+        // import
         "import/extensions": "off",
+        "import/no-extraneous-dependencies": "off",
+        "import/prefer-default-export": "off",
+
+        // react
+        "react/react-in-jsx-scope": "off",
+        "react/jsx-filename-extension": "off",
+        "react/require-default-props": "off",
+        "react/self-closing-comp": "off",
+        "react/jsx-curly-brace-presence": "off",
+        "react/jsx-one-expression-per-line": "off",
+
+        // react-hooks
+        "react-hooks/exhaustive-deps": "off",
+
+        // jsx-a11y
+        "jsx-a11y/click-events-have-key-events": "off",
+        "jsx-a11y/no-noninteractive-element-interactions": "off",
+        "jsx-a11y/no-static-element-interactions": "off",
+
+        // common
         "one-var": "off",
         "indent": ["error", 2],
         "space-in-paren": "off",
         "sort-import": "off",
+        "no-unused-vars": "off",
+        "no-undef": "off",
+        "object-curly-newline": "off",
+        "no-use-before-define": "off",
+        "no-unused-expressions": "off",
+        "operator-linebreak": "off",
+        "no-else-return": "off",
         "no-restricted-imports": [
             "error",
             {
